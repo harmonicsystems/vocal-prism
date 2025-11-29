52a1c489d4dc3257b17471edb8f23a54edaeef97
dnslp <davidnymanslp@gmail.com>
2025-11-28 22:10:39 -0500

    Switch to HashRouter for GitHub Pages compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,19 @@
  * Your voice, refracted through history
  */
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Prism from './pages/Prism';
 import About from './pages/About';
 
 export default function App() {
   return (
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/prism/:f0" element={<Prism />} />
         <Route path="/about" element={<About />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
